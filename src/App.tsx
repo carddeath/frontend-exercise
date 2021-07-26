c9dcb8b64bca587ca5bade55456dc99dc1933ac8
DESKTOP-0HS4U96\Harry <harry.brockley@hotmail.co.uk>
2021-07-26 19:19:16 +0100

    step 2 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { default as React, useState, useEffect } from "react";
 import { StockItem } from "./StockDto";
+import "./App.css";
 
 function App() {
   const [price, setPrice] = useState("0");
+  const [startTime, setStartTime] = useState("");
 
   const getStockPrice = () => {
     fetch("http://34.117.120.204/api/v1/fx/ETHUSD/ohlc")
       .then((response) => response.json())
       .then((stockItem: StockItem) => {
         if (stockItem) {
-          setPrice(stockItem.close);
+          updateStockDetails(stockItem);
         }
       });
   };
@@ -19,6 +21,24 @@ function App() {
       getStockPrice();
     }, 5000);
   }, []);
+
+  const updateStockDetails = (stockItem: StockItem) => {
+    setPrice(stockItem.close);
+
+    const startTimeFormatted = formatStartDate(
+      stockItem.startTime.seconds,
+      stockItem.endTime.seconds,
+    );
+    setStartTime(startTimeFormatted.toISOString());
+  };
+
+  const formatStartDate = (seconds: number, milliseconds: number) => {
+    let freshDate = new Date();
+    freshDate.setSeconds(seconds);
+    freshDate.setMilliseconds(milliseconds);
+
+    return freshDate;
+  };
 
   return (
     <div className="pt-12 bg-gray-50 sm:pt-16">
@@ -35,7 +55,8 @@ function App() {
           <div className="relative px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="max-w-4xl mx-auto">
               <dl className="w-1/3 mx-auto bg-white rounded-lg shadow-lg">
-                <div className="flex flex-col p-6 text-center border-t border-gray-100">
+                <div className="flex flex-col p-6 text-center border-t border-gray-100 tooltip">
+                  <span className="tooltiptext">{startTime}</span>
                   <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
                     ETH/USD
                   </dt>
